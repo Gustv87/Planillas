0ab833d121f42a7f720867e566f6deaab0bf55ff
Gustv87 <tavomartinez87@gmail.com>
2020-06-27 00:48:03 -0600

    se borraron archivos repetidos

--- Planilla.API/app/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const url = 'mongodb://localhost/planillaDb';
-
-mongoose.connect(url)
-    .then(db => console.log('Db esta conectada'))
-    .catch(err => console.error(err));
-
-module.exports = mongoose;    
